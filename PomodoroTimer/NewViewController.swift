0f09c16a78579e0e112843b9955b07d3609cf6bd
Andrey <andreyoala@icloud.com>
2022-05-25 16:48:54 +0300

    Fixed animation for pause/resume state, added metric for code cleaning

--- PomodoroTimer/NewViewController.swift
+++ PomodoroTimer/NewViewController.swift
@@ -1,17 +1,6 @@
-//
-//  NewViewController.swift
-//  PomodoroTimer
-//
-//  Created by Andrey Oala on 21.05.2022.
-//
-
 import UIKit
 
-
 class NewViewController: UIViewController {
-    
-    
-    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -32,19 +21,19 @@ class NewViewController: UIViewController {
         startPauseTimeButton.tintColor = .orange
         startPauseTimeButton.setImage(UIImage(systemName: "play"), for: .normal)
         startPauseTimeButton.imageView?.contentMode = .center
-        startPauseTimeButton.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 40), forImageIn: .normal)
+        startPauseTimeButton.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: Metric.startPauseTimeButtonFontSize), forImageIn: .normal)
         startPauseTimeButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
         return startPauseTimeButton
     }()
 
     private lazy var shapeLayer: CAShapeLayer = {
         let shapeLayer = CAShapeLayer()
-    let shapePosition = CGPoint(x: 200, y: 250)
-        let circularPath = UIBezierPath(arcCenter: shapePosition, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let shapePosition = Metric.layerPositionOnScreen
+        let circularPath = UIBezierPath(arcCenter: shapePosition, radius: Metric.layerRadius, startAngle: Metric.shapeStartAngle, endAngle: Metric.shapeEndAngle, clockwise: true)
      shapeLayer.path = circularPath.cgPath
         shapeLayer.lineCap = CAShapeLayerLineCap.round
      shapeLayer.strokeColor = UIColor.orange.cgColor
-     shapeLayer.lineWidth = 5
+     shapeLayer.lineWidth = Metric.shapeLayerLineWidth
      shapeLayer.strokeEnd = 0
      shapeLayer.fillColor = UIColor.clear.cgColor
         return shapeLayer
@@ -52,12 +41,12 @@ class NewViewController: UIViewController {
     
     private lazy var shadowLayer: CAShapeLayer = {
         let shadowLayer = CAShapeLayer()
-        let shadowLayerPosition = CGPoint(x: 200, y: 250)
-        let shadowLayerPath = UIBezierPath(arcCenter: shadowLayerPosition, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let shadowLayerPosition = Metric.layerPositionOnScreen
+        let shadowLayerPath = UIBezierPath(arcCenter: shadowLayerPosition, radius: Metric.layerRadius, startAngle: Metric.shapeStartAngle, endAngle: Metric.shapeEndAngle, clockwise: true)
         shadowLayer.path = shadowLayerPath.cgPath
         shadowLayer.lineCap = CAShapeLayerLineCap.round
         shadowLayer.strokeColor = UIColor.orange.cgColor
-        shadowLayer.lineWidth = 1
+        shadowLayer.lineWidth = Metric.shadowLayerLineWidth
         shadowLayer.fillColor = UIColor.clear.cgColor
         return shadowLayer
     }()
@@ -67,12 +56,10 @@ class NewViewController: UIViewController {
         timerLabel.text = "\(mins):0\(secs)"
         timerLabel.textColor = .orange
         timerLabel.textAlignment = .center
-        timerLabel.font = .systemFont(ofSize: 45)
+        timerLabel.font = .systemFont(ofSize: Metric.timerLabelFontSize)
         return timerLabel
     }()
-  
-    
-    
+
 // MARK: - Time variables
     lazy var hours: Int = 0
     // timeForTask is also animation duration variable
@@ -81,8 +68,6 @@ class NewViewController: UIViewController {
     lazy var secs: Int = (timeForTask % 60) % 60
     var timer = Timer()
 
-    
-    
 // MARK: - Booleans
     var isWorkingTime: Bool = false
     var timeIsCounting: Bool = false
@@ -97,19 +82,38 @@ class NewViewController: UIViewController {
             timerLabel.text = "\(mins):0\(secs)"
         }
     }
+
+    func pauseLayer(layer : CALayer){
+        let pausedTime : CFTimeInterval = shapeLayer.convertTime(CACurrentMediaTime(), from: nil)
+        shapeLayer.speed = 0.0
+        shapeLayer.timeOffset = pausedTime
+
+    }
+    func resumeAnimation(layer : CALayer){
+        let pausedTime = layer.timeOffset
+        layer.speed = 1.0
+        layer.timeOffset = 0.0
+        layer.beginTime = 0.0
+        let timeSincePause = layer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        layer.beginTime = timeSincePause
+    }
     
     @objc func startButtonTapped() {
-        let startAnimation = CABasicAnimation(keyPath: "strokeEnd")
-               startAnimation.toValue = 1
-                 startAnimation.duration = CFTimeInterval(timeForTask)
-                 startAnimation.fillMode = .forwards
-               startAnimation.isRemovedOnCompletion = false
-               shapeLayer.add(startAnimation, forKey: "urSoBasic")
+        if mins == 5 || mins == 25 && timeIsCounting == false {
+            let startAnimation = CABasicAnimation(keyPath: "strokeEnd")
+                     startAnimation.toValue = 1
+                     startAnimation.duration = CFTimeInterval(timeForTask)
+                     startAnimation.fillMode = .forwards
+                     startAnimation.isRemovedOnCompletion = false
+                     shapeLayer.add(startAnimation, forKey: "urSoBasic")
+        }
         if timeIsCounting == false {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
         startPauseTimeButton.setImage(UIImage(systemName: "pause"), for: .normal)
         timeIsCounting = true
+            resumeAnimation(layer: shapeLayer)
         } else {
+            pauseLayer(layer: shapeLayer)
             timer.invalidate()
             timeIsCounting = false
             startPauseTimeButton.setImage(UIImage(systemName: "play"), for: .normal)
@@ -166,8 +170,6 @@ class NewViewController: UIViewController {
         updateLabel()
     }
     
-
-    
     //MARK: - Settings
     private func setupHierarchy() {
         view.layer.addSublayer(shadowLayer)
@@ -178,13 +180,30 @@ class NewViewController: UIViewController {
     
     private func setupLayout() {
         startPauseTimeButton.translatesAutoresizingMaskIntoConstraints = false
-        startPauseTimeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 280).isActive = true
-        startPauseTimeButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 180).isActive = true
+        startPauseTimeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: Metric.startPauseTimeButtonTopAnchor).isActive = true
+        startPauseTimeButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Metric.startPauseTimeButtonLeftAnchor).isActive = true
         
         timerLabel.translatesAutoresizingMaskIntoConstraints = false
-        timerLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 180).isActive = true
-        timerLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 135).isActive = true
-        timerLabel.widthAnchor.constraint(equalToConstant: 130).isActive = true
+        timerLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: Metric.timerLabelTopAnchor).isActive = true
+        timerLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Metric.timerLabelLeftAnchor).isActive = true
+        timerLabel.widthAnchor.constraint(equalToConstant: Metric.timerLabelWidthAnchor).isActive = true
       
     }
+    // MARK: - Metrics
+    enum Metric {
+        static let startPauseTimeButtonTopAnchor: CGFloat = 280
+        static let startPauseTimeButtonLeftAnchor: CGFloat = 180
+        static let timerLabelTopAnchor: CGFloat = 180
+        static let timerLabelLeftAnchor: CGFloat = 135
+        static let timerLabelWidthAnchor: CGFloat = 130
+        static let startPauseTimeButtonFontSize: CGFloat = 40
+        static let layerPositionOnScreen = CGPoint(x: 200, y: 250)
+        static let layerRadius: CGFloat = 100
+        static let shapeStartAngle = -CGFloat.pi / 2
+        static let shapeEndAngle = 2 * CGFloat.pi
+        static let shapeLayerLineWidth: CGFloat = 5
+        static let shadowLayerLineWidth: CGFloat = 1
+        static let timerLabelFontSize: CGFloat = 45
+    }
+    
 }
